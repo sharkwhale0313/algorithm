e5105f9498829bbff90c9b0e605e6bf67784add3
sharkwhale0313 <71440792+sharkwhale0313@users.noreply.github.com>
2022-02-13 21:46:36 +0900

    Update H-index.py

--- H-index.py
+++ H-index.py
@@ -1,1 +1,12 @@
+# 프로그래머스 H-index
+# 문제 분류 : 정렬
 
+def solution(citations):
+    answer = 0
+    n = len(citations)
+    citations.sort()
+    for h in range(n+1):
+        for i, c in enumerate(citations):
+            if c >= h and n - i >= h:
+                answer = max(answer, h)
+    return answer
